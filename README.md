f33cc24da235b4472b727fa99d68201920a6486b
eshan-sud <eshansud22@gmail.com>
2025-01-29 01:01:14 +0530

    Made progress on the build script

--- README.md
+++ README.md
@@ -11,25 +11,39 @@ Mapping of indoor environments using RGB-D SLAM algorithm on a Raspberry Pi 5 wi
 ## Software Required
 
 1. ROS2 (Humble Hawksbill)
-2. Rospy
-3. Ubuntu 20.04 (Focal)
-4. Eigen3 (3.3.7)
-5. Pangolin (4.5.0)
-6. OpenGL ()
-7. OpenCV (4.25)
-8. CMAKE (3.25.1)
-9. ORB-SLAM3
-10. Raspian OS
-11. 
+2. Ubuntu 20.04 (Focal)
+3. Eigen3 (3.3.7)
+4. Pangolin (4.5.0)
+5. OpenGL ()
+6. OpenCV (4.25)
+7. CMAKE (3.25.1)
+8. ORB-SLAM3
+9. Raspian OS
+10. 
 
 ## Hardware Used
 
 1. Raspberry Pi 5 ()
 2. Raspberry Pi Fan & Heatsink
-3. Microsoft Kinect V1
-4. External Web Camera
+3. Raspberry Pi Power Adaptor (minimum 27 Watts)
+4. 256 GB Mircro SD Card
+5. Microsoft Kinect V1
+6. External Web Camera
 
 
-## Contact the author
+## Build the Project
+
+"The build commands in the file build.sh installs all the required software, \
+frameworks and any additional dependencies required by the user automatically"
+
+- Run these conmmands on the shell
+
+	cd ~/minor-project/
+	chmod +x build.sh
+ 	./build.sh
+ 		- When prompted to choose mode when changin GCC version to 11 ; Enter the value '0'
+
+
+## Ccontact the author
 
 <email to="mailto:eshansud22@gmail.com">eshansud22@gmail.com</email>
--- build.sh
+++ build.sh
@@ -1,95 +1,146 @@
 
 # Clone required repositories
-
-git clone https://github.com/opencv/opencv.git
-git clone https://github.com/opencv/opencv_contrib.git
-git clone https://github.com/ozandmrz/ros2_raspberry_pi_5
-git clone --recursive https://github.com/stevenlovegrove/Pangolin.git
-mkdir -p ~/minor-project/src
-git clone https://github.com/egdw/ORB_SLAM3_Ubuntu20.04 ~/minor-project/ORB_SLAM3
-git clone https://github.com/ozandmrz/orb_slam3_ros2_mono_publisher.git ~/minor-project/ORB_SLAM3/src
+#mkdir -p ~/minor-project/OpenCV
+#git clone https://github.com/opencv/opencv.git ~/minor-project/OpenCV/opencv
+#git clone https://github.com/opencv/opencv_contrib.git ~/minor-project/OpenCV/opencv_contrib
+#git clone https://github.com/ozandmrz/ros2_raspberry_pi_5
+#git clone --recursive https://github.com/stevenlovegrove/Pangolin.git
+#mkdir -p ~/minor-project/ORB_SLAM3/src
+#git clone https://github.com/egdw/ORB_SLAM3_Ubuntu20.04 ~/minor-project/ORB_SLAM3/ORB_SLAM3_Ubuntu20.04
+#git clone https://github.com/ozandmrz/orb_slam3_ros2_mono_publisher.git \
+#~/minor-project/ORB_SLAM3/src/orb_slam3_ros2_mono_publisher
 
 
 # Install dependencies
-sudo apt-get update
-sudo apt update && sudo apt upgrade -y
-sudo apt-get install -y build-essential cmake git libgtk2.0-dev pkg-config \
-libavcodec-dev libavformat-dev libswscale-dev libtbb2 libtbb-dev libjpeg-dev \
-libpng-dev libtiff-dev libdc1394-22-dev libxvidcore-dev libx264-dev libatlas-base-dev \
-gfortran python3-dev libeigen3-dev libboost-all-dev libsuitesparse-dev libopencv-dev \
-libglew-dev libpython2.7-dev python2.7 libboost-python-dev libsdl2-dev mesa-utils \
-glew-utils libepoxy-dev software-properties-common
+#echo "Starting installation of dependencies..."
+#sudo apt-get update
+#sudo apt update && sudo apt upgrade -y
+#sudo apt-get install -y build-essential cmake git libgtk2.0-dev pkg-config \
+#libavcodec-dev libavformat-dev libswscale-dev libtbbmalloc2 libtbb-dev libjpeg-dev \
+#libpng-dev libtiff-dev libdc1394-22-dev libxvidcore-dev libx264-dev libatlas-base-dev \
+#gfortran python3-dev libeigen3-dev libboost-all-dev libsuitesparse-dev libopencv-dev \
+#libglew-dev python3-dev  libboost-python-dev libsdl2-dev mesa-utils glew-utils \
+#libepoxy-dev software-properties-common libboost-serialization-dev libeigen3-dev \
+#libsuitesparse-dev libboost-all-dev cmake g++ libpangolin-dev libssl-dev
 
+#echo "Dependencies installed"
 
 
 # Downgrade GCC Version to 11
-sudo apt-get update
-sudo apt-get install gcc-11 g++-11
-sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-11 60 --slave /usr/bin/g++ g++ /usr/bin/g++-11
-sudo update-alternatives --config gcc
-0 # Choose auto mode (0) to set /usr/bin/gcc-11
-
-gcc --version # Verify gcc Version
-
+#echo "Starting downgradation of GCC version to 11..."
+#sudo apt-get update
+#sudo apt-get install gcc-11 g++-11
+#sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-11 60 --slave /usr/bin/g++ g++ /usr/bin/g++-11
+#sudo update-alternatives --config gcc # Choose auto mode (0) to set /usr/bin/gcc-11
+#gcc --version # Verify gcc Version
+#echo "GCC version downgraded to 11"
 
 
 # Install OpenCV
-
-cd ~/minor-project/opencv_contrib
-git checkout master
-cd ~/minor-project/opencv
-git checkout master
-cd build
-cmake -D CMAKE_BUILD_TYPE=Release \
-      -D CMAKE_INSTALL_PREFIX=/usr/local \
-      -D OPENCV_EXTRA_MODULES_PATH=~/minor-project/opencv_contrib/modules \
-      -D ENABLE_CXX11=ON ..
-make -j2
-sudo make install
-
-pkg-config --modversion opencv4 # Check OpenCV version
-
-cd ~/minor-project
+#echo "Starting installation of OpenCV..."
+#cd OpenCV/opencv_contrib && git checkout master
+#cd ../opencv/ && git checkout master
+#mkdir -p build
+#cd build
+#cmake -D CMAKE_BUILD_TYPE=Release \
+#      -D CMAKE_INSTALL_PREFIX=/usr/local \
+#      -D OPENCV_EXTRA_MODULES_PATH=~/minor-project/OpenCV/opencv_contrib/modules \
+#      -D ENABLE_CXX11=ON .. \
+#      -D ENABLE_PKG_CONFIG=ON ..
+#make -j2
+#sudo make install
+#opencv_version # Check OpenCV version
+#cd ../../../
+#echo "OpenCV Installed"
 
 
 # Install Pangolin
-
-
-cd Pangolin
-./scripts/install_prerequisites.sh recommended
-cd build && cmake .. -D CMAKE_BUILD_TYPE=Release
-make -j2
-sudo make install
-
-cd ./Pangolin/build/examples/HelloPangolin/HelloPangolin # Verify Pangolin Installation
-
+#echo "Starting installation of Pangolin..."
+#cd Pangolin
+#./scripts/install_prerequisites.sh recommended
+#mkdir -p build && cd build && cmake .. -D CMAKE_BUILD_TYPE=Release
+#make -j2
+#sudo make install
+#cd ./examples/HelloPangolin/HelloPangolin # Verify Pangolin Installation
 # Verify Pangolin dependencies
+#glxinfo | grep "OpenGL version" # OpenGL 
+#dpkg -l | grep glew # GLEW
+#ls /usr/include/eigen3/Eigen # eigen3
+#cd ../../../
+#echo "Pangolin Installed"
 
-# glxinfo | grep "OpenGL version"  # OpenGL 
-# dpkg -l | grep glew  && glewinfo | grep "GLEW_VERSION" # GLEW
-# ls /usr/include/eigen3/Eigen # eigen3
+
+# Install ROS2 (Humble Hawksbill)
+#echo "Starting installation of ROS2..."
+#cd ros2_raspberry_pi_5
+#chmod +x ros2_humble_install.sh
+#./ros2_humble_install.sh
+#echo "source ~/ros2_humble/install/local_setup.bash" >> ~/.bashrc
+#source ~/.bashrc
+#sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-keys F42ED6FBAB17C654
+#echo "deb http://repo.ros2.org/ubuntu/main bookworm main" | sudo tee /etc/apt/sources.list.d/ros2.list
+#sudo apt update
+#sudo apt install python3-pip python3-setuptools python3-colour python3-rosdep python3-ament-package libg2o-dev
 
 
 
 
 # Install ORB_SLAM3
-cd ~/minor-project/ORB_SLAM3/
-cd ORB_SLAM3_Ubuntu20.04
-chmod +x build.sh
-./build.sh
-cmake ..
-make -j2
-rosdep install --from-paths src --ignore-src -r -y
-
+#echo "Starting installation of ORB_SLAM3..."
+#cd ~/minor-project/ORB_SLAM3/
+#cd ORB_SLAM3_Ubuntu20.04
+#echo "Configuring and building Thirdparty/DBoW2 ..."
+#cd Thirdparty/DBoW2
+#mkdir -p build && cd build
+#cmake ..
+#make -j 2
+#cd ../../g2o
+#echo "Configuring and building Thirdparty/g2o ..."
+#mkdir -p build && cd build
+#cmake ..
+#make -j 2
+#cd ../../../
+#echo "Uncompress vocabulary ..."
+#cd Vocabulary
+#tar -xf ORBvoc.txt.tar.gz
+#cd ../
+#echo "Configuring and building ORB_SLAM3 ..."
+#mkdir -p build && cd build
+#cmake ..
+#make -j 2
+#rosdep install --from-paths src --ignore-src -r -y
 
 
 # Downloading the TUM Dataset
+#echo "Starting installation of TUM Dataset..."
+#cd ~/minor-project
+#mkdir -p Datasets/TUM/ && cd Datasets/TUM/
+#wget http://vision.in.tum.de/rgbd/dataset/freiburg1/rgbd_dataset_freiburg1_desk.tgz
+#tar -xvzf rgbd_dataset_freiburg1_desk.tgz
+#cd ~/minor-project/ORB_SLAM3/ORB_SLAM3_Ubuntu20.04/Vocabulary/
+#wget https://github.com/UZ-SLAMLab/ORB_SLAM3/raw/master/Vocabulary/ORBvoc.txt.tar.gz # Download the Vocabulary
+#tar -xvzf ORBvoc.txt.tar.gz # Extract Vocabulary
+#cd ../Examples/Monocular
+./mono_tum ../../Vocabulary/ORBvoc.txt TUM1.yaml ~/minor-project/Dataset/TUM/rgbd_dataset_freiburg1_desk # Execute the mapping process
+#echo "TUM Dataset & its Vocabulary installed"
 
-# (Download the vocabulary)
-cd ~/minor-project/ORB_SLAM3/ORB_SLAM3_Ubuntu20.04/Vocabulary/
-wget https://github.com/UZ-SLAMLab/ORB_SLAM3/raw/master/Vocabulary/ORBvoc.txt.tar.gz
 
-tar -xvf ORBvoc.tar.gz
-cd ~/minor-project/ORB_SLAM3/ORB_SLAM3_Ubuntu20.04/Examples/Monocular
-./mono_tum ../../Vocabulary/ORBvoc.txt TUM1.yaml ~/minor-project/Dataset/TUM/rgbd_dataset_freiburg1_desk
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
