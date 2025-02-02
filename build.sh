d22d6a04ac7a85948bad5eff4de90eed7a6649d3
eshan-sud <eshansud22@gmail.com>
2025-02-02 22:31:28 +0530

    Changed the build.sh structure to incorprate readability

--- build.sh
+++ build.sh
@@ -1,6 +1,12 @@
 
+#echo ""
+#echo "-----------------Starting Installation of SLAM for 3D Mapping...----------------"
+#echo ""
+
 # Clone required repositories
-#echo "Cloning required repositories..."
+#echo ""
+#echo "-----------------Cloning required repositories...-------------------------------"
+#echo ""
 #mkdir -p ~/minor-project/OpenCV
 #mkdir -p ~/ros2_humble/src
 #mkdir -p ~/minor-project/ORB_SLAM3/src
@@ -15,13 +21,17 @@
 # ORB SLAM3
 #git clone https://github.com/egdw/ORB_SLAM3_Ubuntu20.04 ~/minor-project/ORB_SLAM3/ORB_SLAM3_Ubuntu20.04
 # ORB SLAM3 ROS2 Publisher
-#git clone https://github.com/ozandmrz/orb_slam3_ros2_mono_publisher.git \ 
+#git clone https://github.com/ozandmrz/orb_slam3_ros2_mono_publisher.git \
 #~/minor-project/ORB_SLAM3/src/orb_slam3_ros2_mono_publisher
-#echo "Required repositories cloned"
+#echo ""
+#echo "-----------------Required repositories cloned-----------------------------------"
+#echo ""
 
 
 # Install dependencies
-#echo "Starting installation of dependencies..."
+#echo ""
+#echo "-----------------Starting installation of dependencies...-----------------------"
+#echo ""
 #sudo apt-get update
 #sudo apt update && sudo apt upgrade -y
 #sudo apt-get install -y build-essential cmake git libgtk2.0-dev pkg-config \
@@ -31,21 +41,36 @@
 #libglew-dev python3-dev  libboost-python-dev libsdl2-dev mesa-utils glew-utils \
 #libepoxy-dev software-properties-common libboost-serialization-dev libeigen3-dev \
 #libsuitesparse-dev libboost-all-dev cmake g++ libssl-dev
-#echo "Dependencies installed"
+#sudo apt install -y git colcon python3-rosdep2 vcstool wget \
+#python3-flake8-docstrings python3-pip python3-pytest-cov \
+#python3-flake8-blind-except python3-flake8-builtins \
+#python3-flake8-class-newline python3-flake8-comprehensions \
+#python3-flake8-deprecated python3-flake8-import-order \
+#python3-flake8-quotes python3-pytest-repeat python3-pytest-rerunfailures \
+#python3-vcstools libx11-dev libxrandr-dev libasio-dev libtinyxml2-dev
+#echo ""
+#echo "-----------------Dependencies installed-----------------------------------------"
+#echo ""
 
 
 # Downgrade GCC Version to 11
-#echo "Starting downgradation of GCC version to 11..."
+#echo ""
+#echo "-----------------Starting downgradation of GCC version to 11...-----------------"
+#echo ""
 #sudo apt-get update
 #sudo apt-get install -y gcc-11 g++-11
 #sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-11 60 --slave /usr/bin/g++ g++ /usr/bin/g++-11
 #sudo update-alternatives --config gcc # Choose auto mode (0) to set /usr/bin/gcc-11
 #gcc --version # Verify gcc Version
-#echo "GCC version downgraded to 11"
+#echo ""
+#echo "-----------------GCC version downgraded to 11-----------------------------------"
+#echo ""
 
 
 # Install OpenCV4
-#echo "Starting installation of OpenCV..."
+#echo ""
+#echo "-----------------Starting installation of OpenCV...-----------------------------"
+#echo ""
 #cd OpenCV/opencv_contrib && git checkout master
 #cd ../opencv/ && git checkout master
 #mkdir -p build
@@ -59,27 +84,35 @@
 #sudo make install
 #opencv_version # Check OpenCV version
 #cd ../../../
-#echo "OpenCV Installed"
+#echo ""
+#echo "-----------------OpenCV installed------------------------------------------------"
+#echo ""
 
 
 # Install Pangolin
-echo "Starting installation of Pangolin..."
-cd Pangolin
-./scripts/install_prerequisites.sh recommended
-mkdir -p build && cd build && cmake .. -D CMAKE_BUILD_TYPE=Release
-make -j 2
-sudo make install
+#echo ""
+#echo "-----------------Starting installation of Pangolin...----------------------------"
+#echo ""
+#cd Pangolin
+#./scripts/install_prerequisites.sh recommended
+#mkdir -p build && cd build
+#cmake .. -D CMAKE_BUILD_TYPE=Release && make -j 2
+#sudo make install
 #cd ./examples/HelloPangolin/HelloPangolin # Verify Pangolin Installation
 # Verify Pangolin dependencies
 #glxinfo | grep "OpenGL version" # OpenGL 
 #dpkg -l | grep glew # GLEW
 #ls /usr/include/eigen3/Eigen # eigen3
 #cd ../../../
-#echo "Pangolin Installed"
+#echo ""
+#echo "-----------------Pangolin installed----------------------------------------------"
+#echo ""
 
 
 # Install ROS2 (Humble Hawksbill)
-#echo "Starting installations of ROS2 (Humble Hawksbill)..."
+#echo ""
+#echo "-----------------Starting installations of ROS2 (Humble Hawksbill)...------------"
+#echo ""
 #cd ~/ros2_humble
 #sudo apt purge 'ros-*' -y
 #sudo apt autoremove -y
@@ -107,10 +140,14 @@ sudo make install
 #sudo apt --fix-broken install
 #sudo apt install -y python3-pip python3-setuptools python3-colour python3-rosdep python3-ament-package\
 #python3-rospkg-modules python3-rosdistro-modules python3-catkin-pkg-modules
-#echo "ROS2 (Humble Hawksbill) installed"
+#echo ""
+#echo "-----------------ROS2 (Humble Hawksbill) installed-------------------------------"
+#echo ""
 
 # Install Image Transport & CV_Bridge
-#echo "Starting installations of Image Transport & CV_Bridge..."
+#echo ""
+#echo "-----------------Starting installations of Image Transport & CV_Bridge...--------"
+#echo ""
 #cd ~/ros2_humble/src
 #sudo apt update
 #sudo apt install curl gnupg2 lsb-release
@@ -127,34 +164,50 @@ sudo make install
 #cd ../..
 #colcon build --symlink-install
 #source install/setup.bash
-#echo "Image Transport & CV_Bridge installed"
+#echo ""
+#echo "-----------------Image Transport & CV_Bridge installed---------------------------"
+#echo ""
 
 
 # Install ORB_SLAM3
-#echo "Starting installation of ORB_SLAM3..."
+#echo ""
+#echo "-----------------Starting installation of ORB_SLAM3...---------------------------"
+#echo ""
 #cd ~/minor-project/ORB_SLAM3/
 #cd ORB_SLAM3_Ubuntu20.04
-#echo "Configuring and building Thirdparty/DBoW2 ..."
+#echo ""
+#echo "-----------------Configuring and building Thirdparty/DBoW2...--------------------"
+#echo ""
 #cd Thirdparty/DBoW2
 #mkdir -p build && cd build
 #cmake .. && make -j 2
 #cd ../../g2o
-#echo "Configuring and building Thirdparty/g2o ..."
+#echo ""
+#echo "-----------------Configuring and building Thirdparty/g2o...----------------------"
+#echo ""
 #mkdir -p build && cd build
 #cmake .. && make -j 2
 #cd ../../../
-#echo "Uncompress vocabulary ..."
+#echo ""
+#echo "-----------------Uncompress vocabulary...----------------------------------------"
+#echo ""
 #cd Vocabulary && tar -xf ORBvoc.txt.tar.gz
 #cd ../
-#echo "Configuring and building ORB_SLAM3 ..."
+#echo ""
+#echo "-----------------Configuring and building ORB-SLAM3...---------------------------"
+#echo ""
 #mkdir -p build && cd build
 #cmake .. && make -j 2
 #rosdep install --from-paths src --ignore-src -r -y
-#echo "ORB_SLAM3 installed"
+#echo ""
+#echo "-----------------ORB_SLAM3 installed---------------------------------------------"
+#echo ""
 
 
 # Downloading the TUM Dataset
-#echo "Starting installation of TUM Dataset..."
+#echo ""
+#echo "-----------------Starting installation of TUM Dataset...-------------------------"
+#echo ""
 #cd ~/minor-project
 #mkdir -p Datasets/TUM/ && cd Datasets/TUM/
 #wget http://vision.in.tum.de/rgbd/dataset/freiburg1/rgbd_dataset_freiburg1_desk.tgz
@@ -164,15 +217,27 @@ sudo make install
 #tar -xvzf ORBvoc.txt.tar.gz # Extract Vocabulary
 #cd ../Examples/Monocular
 #./mono_tum ../../Vocabulary/ORBvoc.txt TUM1.yaml ~/minor-project/Dataset/TUM/rgbd_dataset_freiburg1_desk # Execute the mapping process
-#echo "TUM Dataset & its Vocabulary installed"
+#echo ""
+#echo "-----------------TUM Dataset & its Vocabulary installed--------------------------"
+#echo ""
 
 
 # Publishing ORB_SLAM3 in ROS2:
-echo "Publishing ORB_SLAM3 in ROS2..."
+#echo ""
+#echo "-----------------Publishing ORB_SLAM3 in ROS2...----------------------------------"
+#echo ""
 
 
-echo "Published ORB_SLAM3 in ROS2"
+#echo ""
+#echo "-----------------Published ORB_SLAM3 in ROS2--------------------------------------"
+#echo ""
 
 
+
+
+
+#echo ""
+echo "-----------------SLAM installed---------------------------------------------------"
+#echo ""
 
 
